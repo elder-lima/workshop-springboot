82961b7bbe437caa40ba08fc2610d3dd22213fc3
Elder Lima <eldersantoss1981@gmail.com>
2025-09-10 13:32:11 -0300

    Service layer, component registration

---
+++ src/main/java/com/elderlima/api/services/UserService.java
@@ -0,0 +1,27 @@
+package com.elderlima.api.services;
+
+import com.elderlima.api.entities.User;
+import com.elderlima.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
