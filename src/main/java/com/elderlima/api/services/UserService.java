983fdbe185fc8ac828079300e23c226e2e6e03cd
Elder Lima <eldersantoss1981@gmail.com>
2025-10-18 20:08:25 -0300

    Exception handling - update

--- src/main/java/com/elderlima/api/services/UserService.java
+++ src/main/java/com/elderlima/api/services/UserService.java
@@ -4,6 +4,7 @@ import com.elderlima.api.entities.User;
 import com.elderlima.api.repositories.UserRepository;
 import com.elderlima.api.services.exceptions.DatabaseException;
 import com.elderlima.api.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id " + id);
+        }
     }
 
     private void updateData(User entity, User obj) {
